d9d52b13b1368ba8a9435dee0e9d9b4890ec6cfb
brsaca <brsaca@gmail.com>
2023-09-07 10:10:02 -0600

    Initial Commit

---
+++ XCOrnamentInventory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCOrnamentInventory
+//
+//  Created by Brenda Saavedra Cantu on 07/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCOrnamentInventory/XCOrnamentInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCOrnamentInventoryApp.swift
+//  XCOrnamentInventory
+//
+//  Created by Brenda Saavedra Cantu on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct XCOrnamentInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
